1167256218fbbe3744625f5d9bf942579c5f2bbe
Sudipta <sudipta.g31@gmail.com>
2024-06-12 16:03:54 +0530

    Basic Login/Logut/Signup/Authentication details added

---
+++ upgrad-eshop/src/components/logout/Logout.jsx
@@ -0,0 +1,51 @@
+import Button from "@mui/material/Button";
+import useAuthentication from "../../hooks/useAuthentication";
+
+
+import {clearAllMetadata} from "../../store/actions/metadataAction";
+import {connect} from "react-redux";
+import {useContext} from "react";
+import {useNavigate} from "react-router-dom";
+
+
+const Logout = ({sx, resetMetadata}) => {
+
+	const {AuthCtx} = useAuthentication();
+	const {logout} = useContext(AuthCtx);
+
+	if(sx === null || sx === undefined) {
+		sx = {};
+	}
+	const navigate = useNavigate();
+
+	let performLogout = () => {
+		resetMetadata();
+		logout().then(() => {
+			navigate("/login");
+		});
+	}
+
+	return (
+		<Button sx={sx}
+				variant="contained"
+				color="secondary"
+				onClick={() => performLogout()}>
+			LOGOUT
+		</Button>
+	);
+};
+
+const mapStateToProps = (state) => {
+	return {
+		sortBy: state.metadata.selectedSortBy,
+		category: state.metadata.selectedCategory,
+	};
+};
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		resetMetadata: () => dispatch(clearAllMetadata()),
+	};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Logout);
