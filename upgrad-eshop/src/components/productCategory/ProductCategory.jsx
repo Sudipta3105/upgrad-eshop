2ebadb6923b33f5e5c1c78f394c9f7cabe70c9fd
Sudipta <sudipta.g31@gmail.com>
2024-06-13 20:08:41 +0530

    Added components like Products Details , Products Listing, Product Sorting resp

---
+++ upgrad-eshop/src/components/productCategory/ProductCategory.jsx
@@ -0,0 +1,73 @@
+import {clearFilter, setFilter} from "../../store/actions/metadataAction";
+
+import {connect} from "react-redux";
+
+import {ToggleButton, ToggleButtonGroup} from "@mui/material";
+
+import {useEffect, useState} from "react";
+
+const ProductCategory = ({filter, categories, changeFilter, removeFilter}) => {
+	const [filterValue, setFilterValue] = useState(filter || "ALL");
+
+	useEffect((newValue = filterValue, array = categories) => {
+		let exists = false;
+		for(let i = 0; i < array.length; i++) {
+			if(array[i] === newValue) {
+				exists = true;
+				break;
+			}
+		}
+		if(!exists) {
+			clearFilter();
+			setFilterValue("ALL");
+		}
+	}, [filterValue, categories]);
+	
+	const handleAlignment = (event, newAlignment) => {
+		if(newAlignment != null) {
+			setFilterValue(newAlignment);
+			if (newAlignment === "ALL") {
+				removeFilter();
+			} else {
+				changeFilter(newAlignment);
+			}
+		}
+	};
+
+	return (
+		<ToggleButtonGroup
+			value={filterValue}
+			exclusive
+			onChange={handleAlignment}
+			aria-label="categories"
+		>
+			{categories !== null && categories.map((element, index) => {
+				return (
+					<ToggleButton
+						key={"category_" + index}
+						value={element}
+						aria-label={element}>
+						{element.toUpperCase()}
+					</ToggleButton>
+				);
+			})}
+		</ToggleButtonGroup>
+	);
+};
+
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		changeFilter: (category) => dispatch(setFilter(category)),
+		removeFilter: () => dispatch(clearFilter()),
+	};
+};
+
+const mapStateToProps = (state) => {
+	return {
+		filter: state.metadata.selectedCategory,
+		categories: state.metadata.categories,
+	};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductCategory);
